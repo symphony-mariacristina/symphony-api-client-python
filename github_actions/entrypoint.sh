ef50d2ebb30488d19b719489ce3deb8b85d0db89
symphony-mariacristina <maria.dominicis@symphony.com>
2021-03-09 11:38:31 +0100

    cclausegithubaction

--- github_actions/entrypoint.sh
+++ github_actions/entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-set -e
-
-echo "#################################################"
-echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
-
-sh -c "$*"
-
-echo "#################################################"
-echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+##!/bin/bash
+#set -e
+#
+#echo "#################################################"
+#echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+#
+#sh -c "$*"
+#
+#echo "#################################################"
+#echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
