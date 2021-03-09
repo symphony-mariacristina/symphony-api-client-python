6527e278fbec04128411c3f31240a66c9152eecc
symphony-mariacristina <maria.dominicis@symphony.com>
2021-03-09 18:00:51 +0100

    new test

--- symphony/bdk/core/service/pagination.py
+++ symphony/bdk/core/service/pagination.py
@@ -7,18 +7,18 @@ T = TypeVar('T')
 
 
 async def offset_based_pagination(func: Callable[[int, int], Awaitable[T]],
-                                  chunk_size=50, maxNumber=None) -> AsyncGenerator[T, None]:
+                                  chunk_size=50, max_number=None) -> AsyncGenerator[T, None]:
     """Creates an asynchronous generator from a paginated endpoint. The generator makes the call to the underlying
     endpoint `func` until the `max_number` of items is reached or results are exhausted (i.e. `func` is None or empty).
 
     :param func: a coroutine taking two int parameters: `skip` (a.k.a offset) and `limit` (max number for items to
       retrieve in one call) and returning a list.
     :param chunk_size: the maximum number of elements to retrieve in one call.
-    :param maxNumber: the maximum total number of items to retrieve. If not specified or set to None, it will fetch all
+    :param max_number: the maximum total number of items to retrieve. If not specified or set to None, it will fetch all
       items until we retrieved all elements.
     :return: an asynchronous generator of elements which makes the calls to `func` with the correct parameters.
     """
-    if maxNumber is not None and maxNumber <= 0:
+    if max_number is not None and max_number <= 0:
         return
 
     skip = 0
@@ -30,7 +30,7 @@ async def offset_based_pagination(func: Callable[[int, int], Awaitable[T]],
             yield item
 
             item_count += 1
-            if maxNumber is not None and item_count == maxNumber:
+            if max_number is not None and item_count == max_number:
                 # max_number items already retrieved
                 return
 
